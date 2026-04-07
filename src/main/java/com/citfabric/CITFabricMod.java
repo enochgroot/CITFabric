10d665083554a7815c86f28a37a62c25dd08fd6b
Enoch Groot <80503703+enochgroot@users.noreply.github.com>
2026-04-07 13:39:21 -0400

    v4: rewrite CITFabricMod.java

--- src/main/java/com/citfabric/CITFabricMod.java
+++ src/main/java/com/citfabric/CITFabricMod.java
@@ -6,10 +6,13 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
 
 public class CITFabricMod implements ClientModInitializer {
+    public static final String VERSION = "4.0.0";
+
     @Override
     public void onInitializeClient() {
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES)
             .registerReloadListener(CITManager.INSTANCE);
-        System.out.println("[CITFabric] v1.0.0 loaded");
+        System.out.println("[CITFabric] v" + VERSION + " initialized for MC 1.21.11");
+        System.out.println("[CITFabric] Will scan: optifine/cit, citresewn/cit, mcpatcher/cit");
     }
 }
